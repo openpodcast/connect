b6742c0bbb2be347186a49933b3bdb6c13c36a27
Wolfgang Gassler <wolfgang@gassler.org>
2023-05-10 17:11:23 +0200

    basic api working

--- api/src/dataSources/AnchorConnect.ts
+++ api/src/dataSources/AnchorConnect.ts
@@ -1,45 +1,65 @@
 import axios from 'axios'
 import { RawAxiosResponseHeaders, AxiosResponseHeaders } from 'axios'
+import { AuthError } from '../types/api'
+import setCookie from 'set-cookie-parser'
 
 class AnchorConnect {
     baseURL: string = 'https://podcasters.spotify.com/pod/api/'
 
     constructor() {}
 
-    getSetCookie(
-        headers: RawAxiosResponseHeaders | AxiosResponseHeaders,
-        name: string
-    ): string | undefined {
-        const cookies = headers['set-cookie'] || undefined
-        if (cookies !== undefined) {
-            const matchingCookies = cookies
-                .map((cookie: string) => {
-                    const cookieString = cookie.split(';')[0]
-                    const cookieName = cookieString.split('=')[0]
-                    const cookieValue = cookieString.split('=')[1]
-                    if (cookieName === name) {
-                        return cookieValue
-                    } else {
-                        return undefined
-                    }
-                })
-                .filter((cookie: string | undefined) => cookie !== undefined)
-            return matchingCookies[0]
+    async getSessionData(email: string, password: string): Promise<any> {
+        // login first and get session cookie
+        // then fetch web station id and return eveything in an object
+        try {
+            const sessionCookie = await this.getSession(email, password)
+            const webStationId = await this.getWebStationId(sessionCookie)
+            return {
+                anchorpw_s: sessionCookie,
+                webStationId: webStationId,
+            }
+        } catch (err) {
+            console.log(err)
+            if (err instanceof AuthError && err.status) {
+                throw err
+            } else {
+                throw new Error('Failed to get session data')
+            }
         }
-        return undefined
     }
 
-    async getSession(email: string, password: string): Promise<string> {
+    private getSetCookie(
+        headers: RawAxiosResponseHeaders | AxiosResponseHeaders,
+        name: string,
+        path?: string
+    ): string | undefined {
+        const cookies = setCookie.parse(headers['set-cookie'])
+        const cookie = cookies.find(
+            (cookie) =>
+                cookie.name === name &&
+                (path === undefined || cookie.path === path)
+        )
+        return cookie?.value
+    }
+
+    private async getSession(email: string, password: string): Promise<string> {
         // request crsf token from json body and anchorpw_S cookie from https://podcasters.spotify.com/pod/api/csrf
         const response = await axios.get(this.baseURL + 'csrf')
         const csrfToken = response.data.csrfToken || undefined
-        const anchorpw_S = this.getSetCookie(response.headers, 'anchorpw_S')
+        const anchorpw_s = this.getSetCookie(
+            response.headers,
+            'anchorpw_s',
+            '/'
+        )
 
-        if (csrfToken === undefined || anchorpw_S === undefined) {
-            throw new Error('Failed to get csrf token or anchorpw_S cookie')
+        if (csrfToken === undefined || anchorpw_s === undefined) {
+            throw new Error('Failed to get csrf token or anchorpw_s cookie')
         }
-
-        // login using username, password, csrf token, and anchorpw_S cookie
+        console.log(
+            `csrfToken: ${csrfToken} email ${email} password ${password} anchorpw_s ${anchorpw_s}`
+        )
+        // login using username, password, csrf token, and anchorpw_s cookie
+        // send all three as json payload
         const loginResponse = await axios.post(
             this.baseURL + 'login',
             {
@@ -49,20 +69,44 @@ class AnchorConnect {
             },
             {
                 headers: {
-                    Cookie: `anchorpw_S=${anchorpw_S}`,
+                    Cookie: `anchorpw_s=${anchorpw_s}`,
                 },
+                // do not fail on errors
+                validateStatus: (status) => true,
             }
         )
         // if return status code is 200, we can consider this cookie/session as logged in
         if (loginResponse.status === 200) {
-            const cookie = this.getSetCookie(loginResponse.headers, 'anchor_S')
+            const cookie = this.getSetCookie(
+                loginResponse.headers,
+                'anchorpw_s'
+            )
             if (cookie === undefined) {
                 throw new Error('Failed to get cookie')
             }
             return cookie
+        } else if (loginResponse.status === 403) {
+            const err = new AuthError('Invalid credentials')
+            err.status = 403
+            throw err
         } else {
-            throw new Error('Login failed')
+            throw new Error(
+                'Login failed with status code ' + loginResponse.status
+            )
         }
+    }
+
+    // fetch the web station id (=podcast id) from endpoint podcast/metadata
+    private async getWebStationId(
+        sessionCookie: string
+    ): Promise<string | undefined> {
+        const response = await axios.get(this.baseURL + 'podcast/metadata', {
+            headers: {
+                Cookie: `anchorpw_s=${sessionCookie}`,
+            },
+        })
+        const webStationId = response.data.webStationId || undefined
+        return webStationId
     }
 }
 
--- api/src/index.ts
+++ api/src/index.ts
@@ -8,16 +8,16 @@ import { AnchorConnect } from './dataSources/AnchorConnect'
 
 const config = new Config()
 
-const pool = mysql.createPool({
-    uri: config.getMySQLConnectionString(),
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    multipleStatements: true,
-    // do not touch dates and return native string representation
-    // see https://github.com/mysqljs/mysql#connection-options
-    dateStrings: true,
-})
+// const pool = mysql.createPool({
+//     uri: config.getMySQLConnectionString(),
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0,
+//     multipleStatements: true,
+//     // do not touch dates and return native string representation
+//     // see https://github.com/mysqljs/mysql#connection-options
+//     dateStrings: true,
+// })
 
 const app: Express = express()
 const port = config.getExpressPort()
@@ -31,31 +31,38 @@ app.use(bodyParser.json())
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/connect/:connect-type', (req: Request, res: Response) => {
-    const connectType = req.params['connect-type'] || ''
+app.post('/connect/:connecttype', async (req: Request, res: Response) => {
+    const connectType = req.params['connecttype']
     const connectDataSource = connectDataSources[connectType] || null
+    if (!req.body.email || !req.body.password) {
+        return res.status(400).send('Missing email or password')
+    }
     if (connectDataSource !== null) {
         try {
-            const cookie = connectDataSource.getSession(
+            const sessionData = await connectDataSource.getSessionData(
                 req.body.email,
                 req.body.password
             )
-            res.status(200).send(cookie)
+            res.status(200).send(sessionData)
         } catch (err) {
             console.log(err)
-            res.status(400).send('Invalid credentials')
+            if (err instanceof HttpError && err.status) {
+                res.status(err.status).send(err.message)
+            } else {
+                res.status(500).send(err.message)
+            }
         }
     } else {
         res.status(400).send('Invalid connect type')
     }
 })
 
-app.get(
-    '/health',
-    healthCheck({
-        db: mysqlHealthy(pool),
-    })
-)
+// app.get(
+//     '/health',
+//     healthCheck({
+//         db: mysqlHealthy(pool),
+//     })
+// )
 
 // catch 404 and forward to error handler
 app.use(function (req: Request, res: Response, next: NextFunction) {
