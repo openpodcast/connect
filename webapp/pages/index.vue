f95e12de54aed90c6fea8945d38b363cbb804506
Wolfgang Gassler <wolfgang@gassler.org>
2023-05-12 10:18:10 +0200

    init draft

---
+++ webapp/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+<div>
+  <h1>Welche Plattform willst du an OpenPocast anbinden?</h1>
+  <ul>
+    <li>
+      <nuxt-link to="/anchor">Anchor</nuxt-link>
+    </li>
+  </ul>
+</div>
+</template>
